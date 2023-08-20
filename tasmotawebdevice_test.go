396087614d927dddf00600a647fbddd97bd09b3a
kevinpita <69063372+kevinpita@users.noreply.github.com>
2023-08-20 18:54:55 +0200

    fix: Make linters pass

--- tasmotawebdevice_test.go
+++ tasmotawebdevice_test.go
@@ -1,18 +1,22 @@
-package tasmotamanager
+package tasmotamanager_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/kevinpita/tasmotamanager"
+)
 
 // TODO: Tests should have a mock server that could give a json response so it can be compared too
 
 func TestNoLoginDeviceBackslash(t *testing.T) {
 	const expected = "http://192.168.1.2/cm?cmnd=Power+off"
-	device, errDevice := NewWebDevice("http://192.168.1.2/", "", "")
+	device, errDevice := tasmotamanager.NewWebDevice("http://192.168.1.2/", "", "")
 	if errDevice != nil {
 		t.Errorf("no error was expected when creating the device -> %v", errDevice)
 	}
 
-	device.prepareCommandUrl("Power off")
-	actual := device.Url.String()
+	device.PrepareCommandURL("Power off")
+	actual := device.URL.String()
 
 	if actual != expected {
 		t.Errorf("got %s expected %s", actual, expected)
@@ -21,13 +25,13 @@ func TestNoLoginDeviceBackslash(t *testing.T) {
 
 func TestNoLoginDeviceNoBackslash(t *testing.T) {
 	const expected = "http://192.168.1.2/cm?cmnd=Power+off"
-	device, errDevice := NewWebDevice("http://192.168.1.2", "", "")
+	device, errDevice := tasmotamanager.NewWebDevice("http://192.168.1.2", "", "")
 	if errDevice != nil {
 		t.Errorf("no error was expected when creating the device -> %v", errDevice)
 	}
 
-	device.prepareCommandUrl("Power off")
-	actual := device.Url.String()
+	device.PrepareCommandURL("Power off")
+	actual := device.URL.String()
 
 	if actual != expected {
 		t.Errorf("got %s expected %s", actual, expected)
@@ -36,13 +40,13 @@ func TestNoLoginDeviceNoBackslash(t *testing.T) {
 
 func TestLoginDeviceBackslash(t *testing.T) {
 	const expected = "http://192.168.1.2/cm?cmnd=Power+off&password=pass&user=admin"
-	device, errDevice := NewWebDevice("http://192.168.1.2/", "admin", "pass")
+	device, errDevice := tasmotamanager.NewWebDevice("http://192.168.1.2/", "admin", "pass")
 	if errDevice != nil {
 		t.Errorf("no error was expected when creating the device -> %v", errDevice)
 	}
 
-	device.prepareCommandUrl("Power off")
-	actual := device.Url.String()
+	device.PrepareCommandURL("Power off")
+	actual := device.URL.String()
 
 	if actual != expected {
 		t.Errorf("got %s expected %s", actual, expected)
@@ -51,13 +55,13 @@ func TestLoginDeviceBackslash(t *testing.T) {
 
 func TestLoginDeviceNoBackslash(t *testing.T) {
 	const expected = "http://192.168.1.2/cm?cmnd=Power+off&password=pass&user=admin"
-	device, errDevice := NewWebDevice("http://192.168.1.2", "admin", "pass")
+	device, errDevice := tasmotamanager.NewWebDevice("http://192.168.1.2", "admin", "pass")
 	if errDevice != nil {
 		t.Errorf("no error was expected when creating the device -> %v", errDevice)
 	}
 
-	device.prepareCommandUrl("Power off")
-	actual := device.Url.String()
+	device.PrepareCommandURL("Power off")
+	actual := device.URL.String()
 
 	if actual != expected {
 		t.Errorf("got %s expected %s", actual, expected)
